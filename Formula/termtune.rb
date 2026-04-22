58699b4c1a8122e62aaf3bcd0698bac2ce181c35
Aung Bhone Pyae <aungbhonepyae@MacBook-Pro-2.local>
2026-04-22 11:40:33 +0700

    bump termtune to v0.1.1

--- Formula/termtune.rb
+++ Formula/termtune.rb
@@ -10,8 +10,8 @@
 class Termtune < Formula
   desc "Lightweight terminal YouTube audio player"
   homepage "https://github.com/joistich/termtune"
-  url "https://github.com/joistich/termtune/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f1992c73c544f66e99e28e6c1629e9162d6a842c182d206f86977ef39bd7764b"
+  url "https://github.com/joistich/termtune/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "2a90df216059f5ab004c5b14410936025321060c7bd981657d24b01c60d67559"
   license "MIT"
 
   depends_on "rust" => :build
